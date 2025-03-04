cdd0070272935a93cf1172ab9e424293401015c4
DESKTOP-BPE6J3K\DELL <tong10092003@gmail.com>
2025-03-04 22:59:58 +0700

    Add api-gateway & auth-service and setup docker

---
+++ auth-service/server.js
@@ -0,0 +1,25 @@
+// auth-service/server.js
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const authRoute = require('./routes/authRoute');
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Connected to MongoDB");
+}).catch((err) => {
+    console.error("MongoDB connection error:", err);
+});
+
+app.use('/auth', authRoute);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Auth Service is running on port ${PORT}`);
+});
