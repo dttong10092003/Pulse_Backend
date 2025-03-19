88d18b9476dea8249d986ed3c1783e01d34bd16f
tinph010803 <tinphan924@gmail.com>
2025-03-19 20:02:28 +0700

    update test use be auth/login

--- api-gateway/server.js
+++ api-gateway/server.js
@@ -1,17 +1,26 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");  // Đảm bảo đã cài đặt cors
 
 dotenv.config();
 const app = express();
+
+// Cấu hình CORS cho phép tất cả domain
+app.use(cors({
+  origin: '*', // Cho phép tất cả domain
+  methods: ['GET', 'POST'], // Các phương thức HTTP cho phép
+  allowedHeaders: ['Content-Type', 'Authorization'], // Các header được phép
+}));
+
+// Cấu hình body parser middleware trước khi định nghĩa routes
 app.use(express.json());
 
-// Import routes
+// Import các routes
 const authRoutes = require("./routes/authRoutes");
 const postRoutes = require("./routes/postRoutes");
 const userRoutes = require("./routes/userRoutes");
 const likeRoutes = require("./routes/likeRoutes");
 const commentRoutes = require("./routes/commentRoutes");
-
 
 // Sử dụng routes
 app.use("/auth", authRoutes);
@@ -19,7 +28,6 @@ app.use("/posts", postRoutes);
 app.use("/users", userRoutes);
 app.use("/likes", likeRoutes);
 app.use("/comments", commentRoutes);
-
 
 // API kiểm tra hoạt động của Gateway
 app.get("/", (req, res) => {
