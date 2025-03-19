69437848da5bdf10e9788bf4bf596c0e17c38ebf
DESKTOP-BPE6J3K\DELL <tong10092003@gmail.com>
2025-03-19 22:59:13 +0700

    Add cors

--- api-gateway/server.js
+++ api-gateway/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require('cors');
 
 dotenv.config();
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // Import routes
 const authRoutes = require("./routes/authRoutes");
