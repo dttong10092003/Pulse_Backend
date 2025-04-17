b8a6dd9e5fcc2a720131aed70b0a83b008d74430
DESKTOP-BPE6J3K\DELL <tong10092003@gmail.com>
2025-04-17 20:38:53 +0700

    Update cloudinary

--- chat-service/utils/cloudinary.js
+++ chat-service/utils/cloudinary.js
@@ -2,18 +2,22 @@ const cloudinary = require('cloudinary').v2;
 cloudinary.config();
 
 
-const uploadToCloudinary = async (fileBuffer, folder = 'chat_files') => {
+const uploadToCloudinary = async (buffer, originalFileName, folder = 'chat_files') => {
     return new Promise((resolve, reject) => {
         cloudinary.uploader.upload_stream(
             {
                 folder,  // Tên thư mục trong Cloudinary
+                public_id: originalFileName,
                 resource_type: "auto", // tự động nhận diện loại tài nguyên (ảnh, video, file)
+                use_filename: true,
+                unique_filename: false, // không tạo tên file duy nhất
+                overwrite: true, // ghi đè file nếu đã tồn tại
             },
             (error, result) => {
                 if (error) return reject(error);
                 resolve(result.secure_url);  // Trả về URL an toàn của file đã upload
             }
-        ).end(fileBuffer);  // Đẩy file lên Cloudinary
+        ).end(buffer);  // Đẩy file lên Cloudinary
     });
 };
 
