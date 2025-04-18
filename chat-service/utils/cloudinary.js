16663cba90ff93315fc5dbb789daba9e6d62926e
DESKTOP-BPE6J3K\DELL <tong10092003@gmail.com>
2025-04-18 20:30:03 +0700

    Update cloudinary

--- chat-service/utils/cloudinary.js
+++ chat-service/utils/cloudinary.js
@@ -1,14 +1,22 @@
 const cloudinary = require('cloudinary').v2;
 cloudinary.config();
-
+const path = require('path');
 
 const uploadToCloudinary = async (buffer, originalFileName, folder = 'chat_files') => {
+    const ext = path.extname(originalFileName).toLowerCase();
+    const rawTypes = ['.pdf', '.doc', '.docx', '.xls', '.xlsx', '.zip', '.rar', '.ppt', '.pptx'];
+    const imageTypes = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff', '.webp'];
+
+    let resourceType = 'auto';
+  if (imageTypes.includes(ext)) resourceType = 'image';
+  else if (rawTypes.includes(ext)) resourceType = 'raw';
+
     return new Promise((resolve, reject) => {
         cloudinary.uploader.upload_stream(
             {
                 folder,  // Tên thư mục trong Cloudinary
-                public_id: originalFileName,
-                resource_type: "auto", // tự động nhận diện loại tài nguyên (ảnh, video, file)
+                public_id: path.basename(originalFileName),
+                resource_type: resourceType, // tự động nhận diện loại tài nguyên (ảnh, video, file)
                 use_filename: true,
                 unique_filename: false, // không tạo tên file duy nhất
                 overwrite: true, // ghi đè file nếu đã tồn tại
