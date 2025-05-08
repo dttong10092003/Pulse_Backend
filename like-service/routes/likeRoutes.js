8615c893e65ff1e8634be9b8672fc99ce783f6e8
tinph010803 <tinphan924@gmail.com>
2025-05-08 20:45:46 +0700

    fix routes cuoi

--- like-service/routes/likeRoutes.js
+++ like-service/routes/likeRoutes.js
@@ -2,10 +2,10 @@ const express = require('express');
 const { likePost, unlikePost, getUsersWhoLiked, getUserLikedPosts,getLikeCountsByPosts } = require('../controllers/likeController');
 
 const router = express.Router();
-
+router.post('/count-by-posts', getLikeCountsByPosts);
 router.get('/user-liked-posts', getUserLikedPosts);
 router.post('/:postId', likePost);
 router.delete('/:postId', unlikePost);
 router.get('/:postId', getUsersWhoLiked);
-router.post('/count-by-posts', getLikeCountsByPosts);
+
 module.exports = router;
