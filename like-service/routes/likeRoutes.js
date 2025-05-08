9f16d67c400b50ab5eb594e2dd30c40c390a4b1a
tinph010803 <tinphan924@gmail.com>
2025-05-08 19:36:06 +0700

    update count like

--- like-service/routes/likeRoutes.js
+++ like-service/routes/likeRoutes.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const { likePost, unlikePost, getLikeCount, getUsersWhoLiked, getUserLikedPosts } = require('../controllers/likeController');
+const { likePost, unlikePost, getUsersWhoLiked, getUserLikedPosts,getLikeCountsByPosts } = require('../controllers/likeController');
 
 const router = express.Router();
 
 router.get('/user-liked-posts', getUserLikedPosts);
 router.post('/:postId', likePost);
 router.delete('/:postId', unlikePost);
-router.get('/count/:postId', getLikeCount);
 router.get('/:postId', getUsersWhoLiked);
-
+router.post('/count-by-posts', getLikeCountsByPosts);
 module.exports = router;
